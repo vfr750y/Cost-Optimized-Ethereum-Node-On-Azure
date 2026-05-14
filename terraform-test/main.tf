1c0ea4a47291eda7259a206be398d892202e4a0b
vfr750y <th91843@proton.me>
2026-05-14 12:21:45 +1200

    add github workflows

--- terraform-test/main.tf
+++ terraform-test/main.tf
@@ -1,17 +1,20 @@
 # main.tf (Finalized Dark Node Architecture)
 
-resource "azurerm_resource_group" "eth_node" {
-  name     = "rg-lodestar-node"
-  location = var.location
+variable "location" {
+  default = "australiaeast"
+}
+
+data "azurerm_resource_group" "eth_node" {
+  name = "rg-lodestar-node"
 }
 
 # ---------------------------------------------------------
 # 1. Storage Configuration
 # ---------------------------------------------------------
 resource "azurerm_storage_account" "storage" {
-  name                     = "stlodestardata${random_string.suffix.result}"
-  resource_group_name      = azurerm_resource_group.eth_node.name
-  location                 = azurerm_resource_group.eth_node.location
+  name                     = "stlodestardata499"
+  resource_group_name      = data.azurerm_resource_group.eth_node.name
+  location                 = data.azurerm_resource_group.eth_node.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
